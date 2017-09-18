9b8c66d26ec432e7051d7a38a831f91e57f7c97d
Matthew Carter <matthewcarter@scalefactor.com>
2017-09-18 15:39:45 -0500

    WIP skeleton component for dwolla instant account verification

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  dwolla: Ember.inject.service("dwolla")
+  dwolla: Ember.inject.service("dwolla"),
+
+  setupController(controller, route) {
+    this._super(...arguments);
+    this.get('dwolla').addBankAccount('fake-token', {}).then((response) => {
+      console.log("Created bank account");
+      console.log(response);
+    }).catch((e) => {
+      console.log("Caught an error");
+      console.log(e);
+    });
+  }
 });
