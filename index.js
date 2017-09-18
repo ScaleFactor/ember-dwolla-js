830ce9051f0675a8e3c4def2de659d64bc05263b
Matthew Carter <matthewcarter@scalefactor.com>
2017-09-18 13:30:07 -0500

    add and include dwolla.js in vendor lib

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-dwolla-js'
+  name: 'ember-dwolla-js',
+
+  included(app) {
+    this._super.included(app);
+    this.app.import('vendor/dwolla.js', {
+      type: 'vendor'
+    });
+  }
 };
